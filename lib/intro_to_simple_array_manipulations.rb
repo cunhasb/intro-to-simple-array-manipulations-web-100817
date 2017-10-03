2615b569f040fb605ed3472d564a3b33f146a67f
cunhasb CUNHA <cunhasb@aol.com>
2017-10-03 03:54:18 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,48 @@ def using_push(array,string)
   array.push(string)
 end
 
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array,number)
+  array.pop(number)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def using_shift(array,number)
+  array.shift(number)
+end
+
+def using_concat(array_1,array_2)
+  array_1.concat(array_2)
+end
+
+def using_insert(array,element)
+  array.insert(4,element)
+end
+
+def using_uniq(array)
+  array.uniq 
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+def using_delete_at(array,index)
+  array.delete_at(index)
+end
+
+
